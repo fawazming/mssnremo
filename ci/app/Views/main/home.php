d4bc115336018c8810eb43d5ef1b3c837181b3d4
Fawaz <fawazming@gmail.com>
2024-11-26 05:30:03 +0100

    init

---
+++ ci/app/Views/main/home.php
@@ -0,0 +1,182 @@
+
+    <div class="swiper-container swiper-hero dots-over" data-margin="0" data-autoplay="true" data-autoplaytime="5000" data-nav="true" data-dots="true" data-items="1">
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide h-100 bg-overlay bg-overlay-400 bg-dark" style="background-image:url(assets/img/maleUsurah.jpg);">
+            <div class="container h-100">
+              <div class="row h-100">
+                <div class="col-md-10 offset-md-1 col-lg-7 offset-lg-0 col-xl-6 col-xxl-5 text-center text-lg-start justify-content-center align-self-center align-items-start">
+                  <h2 class="display-1 fs-56 mb-4 text-white animate__animated animate__slideInDown animate__delay-1s">Oh my son! Come on board</h2>
+                  <p class="lead fs-23 lh-sm mb-7 text-white animate__animated animate__slideInRight animate__delay-2s">It is time Muslim students wake up from their slumber and take up their responsibility. By acquiring necessary knowledge that will be sufficient for them to serve Allah, and spread Islam using halal methods.</p>
+                </div>
+                <!--/column -->
+              </div>
+              <!--/.row -->
+            </div>
+            <!--/.container -->
+          </div>
+          <!--/.swiper-slide -->
+          <div class="swiper-slide h-100 bg-overlay bg-overlay-400 bg-dark" style="background-image:url(assets/img/femaleUsurah.jpg);">
+            <div class="container h-100">
+              <div class="row h-100">
+                <div class="col-md-10 offset-md-1 col-lg-7 offset-lg-5 col-xl-6 offset-xl-6 col-xxl-5 offset-xxl-6 text-center text-lg-start justify-content-center align-self-center align-items-start">
+                  <h2 class="display-1 fs-56 mb-4 text-white animate__animated animate__slideInDown animate__delay-1s">...but it is your piety that reaches Him</h2>
+                  <p class="lead fs-23 lh-sm mb-7 text-white animate__animated animate__slideInRight animate__delay-2s">In anticipating the second festival in Islam. Muslims should know that it is not the ram that we want to sacrifice that's important but our piety and intention of the sacrifice.</p>
+                  <!-- <div class="animate__animated animate__slideInUp animate__delay-3s"><a href="#" class="btn btn-lg btn-outline-white rounded-pill">Contact Us</a></div> -->
+                </div>
+                <!--/column -->
+              </div>
+              <!--/.row -->
+            </div>
+            <!--/.container -->
+          </div>
+          <!--/.swiper-slide -->
+        </div>
+        <!--/.swiper-wrapper -->
+      </div>
+      <!-- /.swiper -->
+    </div>
+    <!-- /.swiper-container -->
+
+    <section class="wrapper bg-light">
+      <div class="container pt-16 pt-md-18">
+        <div class="row d-flex align-items-start gy-10 mb-10 mb-md-12">
+          <div class="col-lg-5 position-lg-sticky" style="top: 8rem;">
+            <h3 class="display-2 mb-5">About MSSN Remo</h3>
+            <p class="mb-7">MSSN Remo is the Area Council of the Muslim Student Society of Nigeria dedicated to serving the Muslim student community in the Remo area, which includes towns like Sagamu, Ikenne, Iperu, and others, located in Ogun State. We are committed to promoting student welfarism, fostering spiritual growth, and empowering Muslim students to excel academically and contribute positively to society.</p>
+            <a href="about" class="btn btn-lg btn-primary btn-icon btn-icon-end">Read more<i class="uil uil-arrow-up-right"></i></a>
+          </div>
+          <!-- /column -->
+          <div class="col-lg-6 ms-auto">
+            <div class="card bg-soft-fuchsia mb-6">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <img src="assets/img/usrah.png" class="svg-inject icon-svg icon-svg-md text-fuchsia me-5" alt="" />
+                </div>
+                <div>
+                  <h3 class="fs-21 mb-2">Weekly Usrah</h3>
+                  <p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, nulla non metus auctor eget lacinia odio sem aget elit.</p>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+            <div class="card bg-soft-aqua mb-6">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <img src="./assets/img/umuhaat.png" class="svg-inject icon-svg icon-svg-md text-aqua me-5" alt="" />
+                </div>
+                <div>
+                  <h3 class="fs-21 mb-2">Umuhaat</h3>
+                  <p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, nulla non metus auctor eget lacinia odio sem aget elit.</p>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+            <div class="card bg-soft-yellow mb-6">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <img src="./assets/img/mtp.png" class="svg-inject icon-svg icon-svg-md text-yellow me-5" alt="" />
+                </div>
+                <div>
+                  <h3 class="fs-21 mb-2">Mid-term Camps</h3>
+                  <p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, nulla non metus auctor eget lacinia odio sem aget elit.</p>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+            <div class="card bg-soft-leaf mb-6">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <img src="./assets/img/sc.jpg" class="svg-inject icon-svg icon-svg-md text-leaf me-5" alt="" />
+                </div>
+                <div>
+                  <h3 class="fs-21 mb-2">Sisters' Circle</h3>
+                  <p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, nulla non metus auctor eget lacinia odio sem aget elit.</p>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+            <div class="card bg-soft-orange mb-6">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <img src="./assets/img/rp.png" class="svg-inject icon-svg icon-svg-md text-orange me-5" alt="" />
+                </div>
+                <div>
+                  <h3 class="fs-21 mb-2">Ramadan Programmes</h3>
+                  <p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, nulla non metus auctor eget lacinia odio sem aget elit.</p>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+            <!-- <div class="card bg-soft-grape">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <img src="./assets/img/icons/lineal/browser.svg" class="svg-inject icon-svg icon-svg-md text-grape me-5" alt="" />
+                </div>
+                <div>
+                  <h3 class="fs-21 mb-2">Web Design</h3>
+                  <p class="mb-0">Duis mollis gravida commodo id luctus erat porttitor ligula, nulla non metus auctor eget lacinia odio sem aget elit.</p>
+                </div>
+              </div>
+            </div> -->
+            <!-- /.card -->
+          </div>
+          <!-- /column -->
+        </div>
+      </div>
+      <!-- /.container -->
+    </section>
+    <section class="wrapper" style="background-image:url(assets/img/bg.jpg); background-position: center; background-size: cover;">
+      <div class="container py-10 py-md-12 text-center">
+        <div class="row">
+          <div class="col-lg-10 col-xl-9 col-xxl-8 mx-auto text-center">
+            <h2 class="fs-16 text-uppercase text-white mb-3">Spend in the cause of Allah</h2>
+            <h3 class="text-4xl text-white mb-6">"O you who believe! <span class="underline-2 underline-gradient-6"><em>Spend of the good things</em></span>  which you have (legally) earned... " <i>(Quran 2:267).</i> <br>Kindly support our da'wah cause by donating 👇, nothing is too small. Jazakumullahu khayran</h3>
+            <a href="donate" class="btn btn-white">Donate</a>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <section class="wrapper bg-light">
+      <div class="container pt-10 pb-14 pb-md-16">
+        <div class="row">
+          <div class="col-lg-9 col-xl-8 col-xxl-7 mx-auto text-center">
+            <!-- <h2 class="fs-15 text-uppercase text-muted mb-3">Latest Projects</h2> -->
+            <h3 class="display-4">Check out our <span class="underline-3 style-2 yellow">recent social media</span> posts</h3>
+            <p class="fs-15 text-muted mb-3">Download and share with your contacts</p>
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+        <div class="swiper-container grid-view mb-6" data-margin="30" data-dots="true" data-items-xl="3" data-items-md="2" data-items-xs="1">
+          <div class="swiper">
+            <div class="swiper-wrapper">
+              <?php foreach ($smposts as $k => $smpost): ?>
+              <?php if ($k==0):?>
+              <?php else: ?>
+              <div class="swiper-slide">
+                <figure class="rounded mb-6"><img src="<?=$smpost?>" srcset="<?=$smpost?>" alt="" /><a class="item-link" href="<?=$smpost?>" data-glightbox data-gallery="projects-group"><i class="uil uil-focus-add"></i></a></figure>
+                <!-- /.project-details -->
+              </div>
+              <!--/.swiper-slide -->
+            <?php endif; ?>
+            <?php endforeach; ?>
+            </div>
+            <!--/.swiper-wrapper -->
+          </div>
+          <!-- /.swiper -->
+        </div>
+        <!-- /.swiper-container -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
